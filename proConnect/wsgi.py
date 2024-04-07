302a1c1fa6d4838c4ad1ff87f12dafee931a7f91
Brian Isale <isale.bryan@gmail.com>
2024-04-07 10:30:45 +0300

    healthstack>proConnect

--- proConnect/wsgi.py
+++ proConnect/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for healthstack project.
+WSGI config for proConnect project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthstack.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proConnect.settings')
 
 application = get_wsgi_application()
